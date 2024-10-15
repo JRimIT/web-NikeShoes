d497e4b4f9cfb918617370b9d2c588a5a364efd1
JRimIT <rri603024@gmail.com>
2024-10-15 07:44:59 +0700

    merge_by Truonng

--- client/src/layout/LayOut.jsx
+++ client/src/layout/LayOut.jsx
@@ -5,8 +5,6 @@ import HomePageMen from "../components/Home/HomeMen/HomePageMen";
 import HomePageWoman from "../components/Home/HomeWomen/HomePageWoman";
 import ProductListPageMen from "../components/Home/listProduct/men/ProductListPageMen";
 import ProductListPageWomen from "../components/Home/listProduct/women/ProductListPageWomen";
-import Login from "../components/login/Login";
-import Register from "../components/login/Register";
 import ProductDetailPage from "../components/Home/listProduct/Product/ProductDetailPage";
 
 const LayOut = () => {
@@ -15,13 +13,22 @@ const LayOut = () => {
       <Routes>
         {/*Route nested: Cha con lồng nhau  */}
         <Route path="/" element={<App />}>
-          <Route index element={<HomePage/>}></Route>
-          <Route path="/men" element={<HomePageMen/>}></Route>
-          <Route path="/women" element={<HomePageWoman/>}></Route>
-          <Route path="/products-men" element={<ProductListPageMen/>}></Route>
-          <Route path="products-men/:category" element={<ProductListPageMen />} />
-          <Route path="/products-women" element={<ProductListPageWomen/>}></Route>
-          <Route path="products-women/:category" element={<ProductListPageWomen />} />
+          <Route index element={<HomePage />}></Route>
+          <Route path="/men" element={<HomePageMen />}></Route>
+          <Route path="/women" element={<HomePageWoman />}></Route>
+          <Route path="/products-men" element={<ProductListPageMen />}></Route>
+          <Route
+            path="products-men/:category"
+            element={<ProductListPageMen />}
+          />
+          <Route
+            path="/products-women"
+            element={<ProductListPageWomen />}
+          ></Route>
+          <Route
+            path="products-women/:category"
+            element={<ProductListPageWomen />}
+          />
           {/* <Route path="/login" element={<Login/>} />
           <Route path="/register" element={<Register />} /> */}
           <Route path="/products/:id" element={<ProductDetailPage />} />
--- server/server.js
+++ server/server.js
@@ -1,30 +1,32 @@
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
 
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:3000' 
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+  })
+);
 
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',     
-  password: '12345', 
-  database: 'Nike'  
+  host: "localhost",
+  user: "root",
+  password: "02111109",
+  database: "Nike",
 });
 
 db.connect((err) => {
   if (err) {
-    console.log('Error connecting to database:', err);
+    console.log("Error connecting to database:", err);
   } else {
-    console.log('Connected to MySQL database.');
+    console.log("Connected to MySQL database.");
   }
 });
 
-app.get('/products', (req, res) => {
+app.get("/products", (req, res) => {
   const category = req.query.category;
   let query;
   const queryParams = [];
@@ -48,17 +50,18 @@ app.get('/products', (req, res) => {
     }
   } else {
     // Trả về tất cả sản phẩm liên quan đến Men nếu không có category trong query
-    query = 'SELECT * FROM products WHERE category LIKE ? AND category NOT LIKE ?';
-    queryParams.push('%Men%', '%Women%');
+    query =
+      "SELECT * FROM products WHERE category LIKE ? AND category NOT LIKE ?";
+    queryParams.push("%Men%", "%Women%");
   }
 
-  console.log('Executing query:', query);
-  console.log('With parameters:', queryParams);
+  console.log("Executing query:", query);
+  console.log("With parameters:", queryParams);
 
   db.query(query, queryParams, (error, results) => {
     if (error) {
-      console.error('Database query error:', error);
-      return res.status(500).json({ message: 'Error fetching products' });
+      console.error("Database query error:", error);
+      return res.status(500).json({ message: "Error fetching products" });
     }
 
     const totalCount = results.length;
@@ -67,16 +70,18 @@ app.get('/products', (req, res) => {
 });
 
 // New route to fetch product details by ID
-app.get('/products/:id', (req, res) => {
+app.get("/products/:id", (req, res) => {
   const productId = req.params.id;
 
-  const query = 'SELECT * FROM products WHERE product_id = ?'; // Use product_id instead of id
+  const query = "SELECT * FROM products WHERE product_id = ?"; // Use product_id instead of id
   db.query(query, [productId], (error, results) => {
     if (error) {
-      return res.status(500).json({ message: 'Error fetching product details' });
+      return res
+        .status(500)
+        .json({ message: "Error fetching product details" });
     }
     if (results.length === 0) {
-      return res.status(404).json({ message: 'Product not found' });
+      return res.status(404).json({ message: "Product not found" });
     }
     res.json(results[0]); // Return the first product
   });
@@ -86,4 +91,3 @@ const port = 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
