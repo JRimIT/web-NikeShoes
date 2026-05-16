171600782d85fb83927402d9836448c8bb443e68
JRimIT <rri603024@gmail.com>
2026-05-16 22:32:10 +0700

    Switch to MySQL Pool for better stability

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,23 @@
 // server/config/db.js
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "02111109",
   database: process.env.DB_NAME || "Nike",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+// Kiểm tra kết nối
+db.getConnection((err, connection) => {
   if (err) {
     console.error(`Error connecting to MySQL database at ${process.env.DB_HOST || "localhost"}:`, err);
   } else {
     console.log(`Connected to MySQL database at ${process.env.DB_HOST || "localhost"}.`);
+    connection.release();
   }
 });
 
